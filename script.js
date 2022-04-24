f182746d2b3a01fea09d6ec600004ccc749803fb
guchi-hiro <5cj5jh@bma.biglobe.ne.jp>
2022-04-24 12:36:28 +0900

    Add async param

--- script.js
+++ script.js
@@ -82,26 +82,26 @@ const questions = [
   {
     question: '東京メトロといえば?',
     answers: [
-      { value: "ginza" , image: "<img src ='q1/ginza.png' alt='Ginza Line'>"},
-	  { value: "maru" , image: "<img src ='q1/maru.png' alt='Marunouchi Line'>"},
-	  { value: "hibiya" , image: "<img src ='q1/hibiya.png' alt='Hibiya Line'>"},
-	  { value: "tohzai" , image: "<img src ='q1/tohzai.png' alt='Tohzai Line'>"},
-	  { value: "yuraku" , image: "<img src ='q1/yuraku.png' alt='Yurakucho Line'>"},
-	  { value: "fuku" , image: "<img src ='q1/fuku.png' alt='Fukutoshin Line'>"},
-	  { value: "hanzo" , image: "<img src ='q1/hanzo.png' alt='Hanzo Line'>"},
-	  { value: "chiyoda" , image: "<img src ='q1/chiyoda.png' alt='Chiyoda Line'>"},
-	  { value: "namboku" , image: "<img src ='q1/namboku.png' alt='NamBolu Line'>"}
+      { value: "ginza" , image: "<img src ='q1/ginza.png' alt='Ginza Line' decoding='async'>"},
+	  { value: "maru" , image: "<img src ='q1/maru.png' alt='Marunouchi Line' decoding='async'>"},
+	  { value: "hibiya" , image: "<img src ='q1/hibiya.png' alt='Hibiya Line' decoding='async'>"},
+	  { value: "tohzai" , image: "<img src ='q1/tohzai.png' alt='Tohzai Line' decoding='async'>"},
+	  { value: "yuraku" , image: "<img src ='q1/yuraku.png' alt='Yurakucho Line' decoding='async'>"},
+	  { value: "fuku" , image: "<img src ='q1/fuku.png' alt='Fukutoshin Line' decoding='async'>"},
+	  { value: "hanzo" , image: "<img src ='q1/hanzo.png' alt='Hanzo Line' decoding='async'>"},
+	  { value: "chiyoda" , image: "<img src ='q1/chiyoda.png' alt='Chiyoda Line' decoding='async'>"},
+	  { value: "namboku" , image: "<img src ='q1/namboku.png' alt='NamBolu Line' decoding='async'>"}
     ]
   },
   {
     question: 'SNSといえば?',
     answers: [
-      { value: "facebook" , image: "<img src ='q2/facebook.png' alt='Facebook'>"},
-	  { value: "twitter" , image: "<img src ='q2/twitter.png' alt='Twitter'>"},
-	  { value: "insta" , image: "<img src ='q2/insta.png' alt='Insta'>"},
-	  { value: "line" , image: "<img src ='q2/line.png' alt='LINE'>"},
-	  { value: "tiktok" , image: "<img src ='q2/tiktok.png' alt='Tiktok'>"},
-	  { value: "youtube" , image: "<img src ='q2/youtube.png' alt='Youtube'>"}
+      { value: "facebook" , image: "<img src ='q2/facebook.png' alt='Facebook' decoding='async'>"},
+	  { value: "twitter" , image: "<img src ='q2/twitter.png' alt='Twitter' decoding='async'>"},
+	  { value: "insta" , image: "<img src ='q2/insta.png' alt='Insta' decoding='async'>"},
+	  { value: "line" , image: "<img src ='q2/line.png' alt='LINE' decoding='async'>"},
+	  { value: "tiktok" , image: "<img src ='q2/tiktok.png' alt='Tiktok' decoding='async'>"},
+	  { value: "youtube" , image: "<img src ='q2/youtube.png' alt='Youtube' decoding='async'>"}
     ]
   },
   {
@@ -121,27 +121,27 @@ const questions = [
   {
     question: 'あなたのスマホの色は?',
     answers: [
-      { value: "white" , image: "<img src ='q4/white.png' alt='White'>"},
-	  { value: "black" , image: "<img src ='q4/black.png' alt='Black'>"},
-	  { value: "blue" , image: "<img src ='q4/blue.png' alt='Blue'>"},
-	  { value: "red" , image: "<img src ='q4/red.png' alt='Red'>"},
-	  { value: "yellow" , image: "<img src ='q4/yellow.png' alt='Yellow'>"},
-	  { value: "green" , image: "<img src ='q4/green.png' alt='Green'>"},
-	  { value: "purple" , image: "<img src ='q4/purple.png' alt='Purple'>"},
-	  { value: "pink" , image: "<img src ='q4/pink.png' alt='Pink'>"},
-	  { value: "orange" , image: "<img src ='q4/orange.png' alt='Orange'>"}
+      { value: "white" , image: "<img src ='q4/white.png' alt='White' decoding='async'>"},
+	  { value: "black" , image: "<img src ='q4/black.png' alt='Black' decoding='async'>"},
+	  { value: "blue" , image: "<img src ='q4/blue.png' alt='Blue' decoding='async'>"},
+	  { value: "red" , image: "<img src ='q4/red.png' alt='Red' decoding='async'>"},
+	  { value: "yellow" , image: "<img src ='q4/yellow.png' alt='Yellow' decoding='async'>"},
+	  { value: "green" , image: "<img src ='q4/green.png' alt='Green' decoding='async'>"},
+	  { value: "purple" , image: "<img src ='q4/purple.png' alt='Purple' decoding='async'>"},
+	  { value: "pink" , image: "<img src ='q4/pink.png' alt='Pink' decoding='async'>"},
+	  { value: "orange" , image: "<img src ='q4/orange.png' alt='Orange' decoding='async'>"}
     ]
   },
   {
     question: 'プログラミング言語といえば?',
     answers: [
-      { value: "java" , image: "<img src ='q5/java.png' alt='Java'>"},
-	  { value: "c" , image: "<img src ='q5/c.png' alt='C'>"},
-	  { value: "c++" , image: "<img src ='q5/cplus.png' alt='C++'>"},
-	  { value: "python" , image: "<img src ='q5/python.png' alt='Python'>"},
-	  { value: "php" , image: "<img src ='q5/php.png' alt='php'>"},
-	  { value: "ruby" , image: "<img src ='q5/ruby.png' alt='Ruby'>"},
-	  { value: "js" , image: "<img src ='q5/js.png' alt='Java Script'>"}
+      { value: "java" , image: "<img src ='q5/java.png' alt='Java' decoding='async'>"},
+	  { value: "c" , image: "<img src ='q5/c.png' alt='C' decoding='async'>"},
+	  { value: "c++" , image: "<img src ='q5/cplus.png' alt='C++' decoding='async'>"},
+	  { value: "python" , image: "<img src ='q5/python.png' alt='Python' decoding='async'>"},
+	  { value: "php" , image: "<img src ='q5/php.png' alt='php' decoding='async'>"},
+	  { value: "ruby" , image: "<img src ='q5/ruby.png' alt='Ruby' decoding='async'>"},
+	  { value: "js" , image: "<img src ='q5/js.png' alt='Java Script' decoding='async'>"}
     ]
   },
 ]
